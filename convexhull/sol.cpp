2e49db149564face6640147822c3a9d9276ca539
co-jo <colin.hryniowski@unb.ca>
2018-12-16 14:19:28 -0400

    Convex hull, added extra test cases.

---
+++ convexhull/sol.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+struct point {
+    int x, y;
+
+    // First sort by x, and then by y.
+    bool operator <(const point &a) const {
+        return x < a.x || (x == a.x && y < a.y);
+    }
+    bool operator ==(const point &a) const {
+        return x == a.x && y == a.y;
+    }
+};
+
+int cross(point a, point b, point c) {
+    return (a.x - c.x) * (b.y - c.y) - (b.x - c.x) * (a.y - c.y);
+}
+
+vector<point> solve(vector<point> &points) {
+
+    if (points.size() == 1) return points;
+
+    vector<point> hull(2 * (points.size() - 1));
+
+    int index = 0;
+    // Minimum acceptable partition size for the lower half
+    int bound = 2;
+    // L -> R (Upper -- going clockwise)
+    for (int i = 0; i < points.size(); i++) {
+        while(index >= bound && cross(hull[index-1], points[i], hull[index-2]) <= 0) {
+            index--;
+        }
+
+        if (!(points[i] == hull[index - 1])) {
+            hull[index++] = points[i];
+        }
+
+    }
+
+    // Minimum acceptable parition size for the upper half
+    bound = index + 1;
+
+    // R -> L (Lower -- going clockwise)
+    for (int i = points.size() - 1; i > 0; i--) {
+        while(index >= bound && cross(hull[index-1], points[i-1], hull[index-2]) <= 0) {
+            index--;
+        }
+        // Moving from right most point, and heading left
+        //if (!(points[i - 1] == hull[index - 1])) {
+            hull[index++] = points[i-1];
+        //}
+    }
+    //for (int i = 0; i < index; i++) cout << hull[i].x << ' ' << hull[i].y << endl;
+    // Removes all extra zeroes, and the last duplicated point the upper/lower halfs meet.
+    hull.resize(index - 1);
+
+    return hull;
+}
+
+int main() {
+
+    int num;
+    while(cin >> num) {
+
+        if (!num) return 0;
+
+        int x,y;
+        vector<point> points, sorted;
+        for (int i = 0; i < num; i++) {
+            cin >> x >> y;
+            points.push_back({x,y});
+        }
+
+        sort(points.begin(), points.end());
+
+        vector<point> convex = solve(points);
+
+        cout << convex.size() << endl;
+        for (int i = 0; i < convex.size(); i++) {
+            cout << convex[i].x << ' ' << convex[i].y << endl;
+        }
+
+    }
+    return 0;
+}
